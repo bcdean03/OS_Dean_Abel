e58a811bf30c7f4d92ec3716ac1d5452a59f0aef
luludoobee <bcdean03@gmail.com>
2015-11-23 14:59:42 -0700

    Welcome

---
+++ consumer_monitor/ConsumerMonitor.py
@@ -0,0 +1,17 @@
+__author__ = 'Dean'
+import socket
+
+def main():
+    consumer_num = raw_input("How many consumers do you want?")
+    producer_num = raw_input("How many producers do you want to produce?")
+    buffer_size = raw_input("What is the size of the buffer you want to restrict the producers to produce?")
+    s = socket.socket()
+    s.connect(("localhost",5002))#request a connection with the listening server
+    s.send(consumer_num,producer_num,buffer_size)
+    data = s.recv(1024)
+    if data != "received":
+        print "Error happened"
+    s.close()
+
+if __name__ == '__main__':
+    main()
