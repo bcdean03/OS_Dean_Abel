e5fb911e411092a6bbf55453b046562ea8a56876
luludoobee <bcdean03@gmail.com>
2015-11-23 15:32:36 -0700

    Welcome

--- server/ProducerServer.py
+++ server/ProducerServer.py
@@ -3,18 +3,21 @@ import socket
 
 
 def main():
-
-    addr =("192.168.1.141",5002)
-    sock = socket.socket()
-    sock.bind(addr)
-    sock.listen(1)
-    client,client_addr = sock.accept()
-    print "Made connection with client --->",client_addr
-    list_in = sock.recv(1024)
-    print "Received:", list_in
-    sock.send("received")
-    sock.close()
-
+    try:
+        addr= ("192.168.1.141", 5002)
+        sock = socket.socket()
+        sock.bind(addr)
+        sock.listen(1)
+        print "Waiting for connection..."
+        client,client_addr = sock.accept()
+        print "Made connection with client --->",client_addr
+        list_in = client.recv(1024)
+        print "Received:", list_in
+        client.send("received")
+    finally:
+        "Closing socket..."
+        sock.close()
+        client.close()
 
 
 
