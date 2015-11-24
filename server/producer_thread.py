749c92245282892cd1835f803d9f243fd3f80f14
luludoobee <bcdean03@gmail.com>
2015-11-23 17:05:46 -0700

    Hey abel im next to you

--- server/producer_thread.py
+++ server/producer_thread.py
@@ -1,8 +1,29 @@
 __author__ = 'Dean, Abel'
 from threading import Thread
-
-
+# from Queue import Queue
+# from time import sleep
+#
+# q = Queue()
 class Producer(Thread):
     def run(self):
-        print "hello",self.name
-        print "hasdasdasd"
+        # while q.qsize() != 0:
+            print str(q.get()) + " " + self.name
+
+
+# def wait_to_put():
+#     q.put(1)
+#
+#
+# if __name__ == '__main__':
+#     for i in range(0,30):
+#         Producer(name="{}".format(i+1)).start()
+#     sleep(2)
+#     wait_to_put()
+#     wait_to_put()
+#     wait_to_put()
+#     wait_to_put()
+#     wait_to_put()
+#     wait_to_put()
+#     wait_to_put()
+#     wait_to_put()
+#
