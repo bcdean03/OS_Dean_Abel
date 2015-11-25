fce938b6f990aae45c80411beff987c4fc732c72
abelamadou <abelamadou@msudenver.edu>
2015-11-24 19:33:50 -0700

    finishing the consumer

--- server/producer_server.py
+++ server/producer_server.py
@@ -56,7 +56,7 @@ def start_listening():
     addr= ("192.168.1.187", 5007)
     sock = socket.socket()
     sock.bind(addr)
-    sock.listen(100)
+    sock.listen(100000)
     while True:
         print "Waiting for connection from clients..."
         client, client_addr = sock.accept()
