979a11d987468d4b9c16af85c4cd70c92cb7835d
abelamadou <abelamadou@msudenver.edu>
2015-11-26 21:29:43 -0700

    updating color of canvas oval and labels all done

--- Gui_Final_Practice/Gui_Classes/change_label_color.py
+++ Gui_Final_Practice/Gui_Classes/change_label_color.py
@@ -1,11 +1,15 @@
 __author__ = 'abelamadou'
 from Tkinter import *
+
 class DisplayClient:
-    def __init__(self,dot,clientName,food,ingredient):
-        self.dot=dot
+    def __init__(self,canvas,oval,clientName,food,ingredient):
+        self.canvas=canvas
+        self.oval=oval
         self.clientName=clientName
         self.food=food
         self.ingredient=ingredient
-        pass
     def change_label_color(self,l_obj,color):
         l_obj.config(bg=color)
+
+    def change_oval(self,color):
+        self.canvas.itemconfig(self.oval,fill=color)
